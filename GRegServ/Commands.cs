33967ab0a755426a151666d070b9f0df62115f41
Ben Adams <emailbenjaminadams@gmail.com>
2020-07-21 10:23:13 +0100

    Upload Project

---
+++ GRegServ/Commands.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRegServ
+{
+    class Commands
+    {
+        public static List<string> errors = new List<string>();
+        public static List<string> succeeded = new List<string>();
+        public void RegisterDLL()
+        {
+            
+
+            foreach (string file in MainWindow.SelectedFiles)
+            {
+                string CmdText = "/C REGSVR32 /s " + '"' + file + '"';
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = CmdText;
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                //startInfo.RedirectStandardOutput = true;
+                //startInfo.RedirectStandardError = true;
+                //startInfo.UseShellExecute = false;
+                process.StartInfo = startInfo;
+                process.Start();
+                //string result = process.StandardOutput.ReadLine().ToString();
+                //succeeded.Add(result);
+            }
+        }
+
+        public void UnregisterDLL()
+        {
+            foreach (string file in MainWindow.SelectedFiles)
+            {
+                string CmdText = "/C REGSVR32 /u " + '"' + file + '"';
+                System.Diagnostics.Process process2 = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo2 = new System.Diagnostics.ProcessStartInfo();
+                startInfo2.FileName = "cmd.exe";
+                startInfo2.Arguments = CmdText;
+                startInfo2.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                process2.StartInfo = startInfo2;
+                process2.Start();
+
+            }
+        }
+
+    }
+}
---
+++ GRegServ/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GRegServ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        Commands File = new Commands();
+        public static List<string> SelectedFiles = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ofd.InitialDirectory = @"C:\";
+            ofd.Title = "Select The File(s) You Want to Register";
+            ofd.Multiselect = true;
+        }
+
+        private void btnSelectFiles_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedFiles.Clear();
+            lstSelectedFiles.Items.Clear();
+
+            ofd.ShowDialog();
+            foreach (string file in ofd.FileNames)
+            {
+                SelectedFiles.Add(file);
+                lstSelectedFiles.Items.Add(file);
+            }
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            File.RegisterDLL();
+            lstSelectedFiles.Items.Clear();
+            SelectedFiles.Clear();
+            //lstSelectedFiles.ItemsSource = Commands.succeeded;
+        }
+
+        private void btnUnregister_Click(object sender, RoutedEventArgs e)
+        {
+            File.UnregisterDLL();
+            lstSelectedFiles.Items.Clear();
+            SelectedFiles.Clear();
+        }
+    }
+}
